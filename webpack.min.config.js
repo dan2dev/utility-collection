5553043061de58f3dffcb93932703eeae714006d
Danilo C Castro <ccastro.danilo@gmail.com>
2018-01-24 14:29:34 -0300

    update and fixes

--- webpack.min.config.js
+++
@@ -1,9 +0,0 @@
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var webpack = require("webpack");
-var config = require("./webpack.config.js");
-
-config.output.filename = "[name].min.js";
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-}));
-
-module.exports = config;
