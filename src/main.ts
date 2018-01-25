6bd069a0c0da2d342c4d9c2c484a6c04851d442a
Danilo C Castro <ccastro.danilo@gmail.com>
2018-01-24 21:09:22 -0300

    fix export

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-export * from "./dom";
-export * from "./is";
-export * from "./string";
-export * from "./reflection";
-export * from "./url";
-export * from "./list";
-export * from "./scroll-switch";
-export * from "./event";
+import { Dom } from "./dom";
+import { Is } from "./is";
+import { String } from "./string";
+import { Reflection } from "./reflection";
+import { Url } from "./url";
+import { List } from "./list";
+import { ScrollSwitch } from "./scroll-switch";
+import { Event } from "./event";
 export namespace UtilityCollection {
 	const name: string = "UtilityCollection";
 }
