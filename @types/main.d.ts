831d8802b5a4fbdb86df87b61a51a33d3538562e
Danilo C Castro <ccastro.danilo@gmail.com>
2018-01-24 21:23:38 -0300

    update fix export

--- @types/main.d.ts
+++ @types/main.d.ts
@@ -1,11 +1,3 @@
-export * from "./dom";
-export * from "./is";
-export * from "./string";
-export * from "./reflection";
-export * from "./url";
-export * from "./list";
-export * from "./scroll-switch";
-export * from "./event";
 export declare namespace UtilityCollection {
 }
 export default UtilityCollection;
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { Url } from "./url";
 import { List } from "./list";
 import { ScrollSwitch } from "./scroll-switch";
 import { Event } from "./event";
+
+exports.Dom = Dom;
+exports.Is = Is;
+exports.String = String;
+exports.Reflection = Reflection;
+exports.Url = Url;
+exports.List = List;
+exports.ScrollSwitch = ScrollSwitch;
+exports.Event = Event;
+
 export namespace UtilityCollection {
 	const name: string = "UtilityCollection";
 }
